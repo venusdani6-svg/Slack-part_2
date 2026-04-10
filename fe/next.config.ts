523f27a2a5787e96651c8596df4e8b0fb570b681
venusdani6-svg <venusdani6@gmail.com>
2026-04-10 10:36:22 +0900

    Update next.config.ts
    fix Ip

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   allowedDevOrigins: ['192.168.137.37'],
+   allowedDevOrigins: ['192.168.137.106'],
   /* config options here */
     webpack: (config) => {
     config.cache = false;
